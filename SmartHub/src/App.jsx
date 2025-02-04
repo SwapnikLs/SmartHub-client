616a79b44e8c0c56b24a1d1d09a4fad0e3939a4a
Swapnik Ls <swapniklatupally@gmail.com>
2025-02-04 22:06:28 +0530

    working clean

--- SmartHub/src/App.jsx
+++ SmartHub/src/App.jsx
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import AboutUs from './Components/AboutUs';
 import AuthContainer from './Components/LearnWithUs';
-=======
->>>>>>> 317c10131681380ed960efe6df7e9905f2e128b8
 import LandingPage from './Pages/LandingPage';
 function App() {
   
