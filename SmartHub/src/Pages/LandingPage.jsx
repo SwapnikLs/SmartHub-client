56a9e49c7681859e2a0e47d1686c804debafd6a1
Swapnik Ls <swapniklatupally@gmail.com>
2025-02-04 22:01:29 +0530

    tracked all the files

--- SmartHub/src/Pages/LandingPage.jsx
+++ SmartHub/src/Pages/LandingPage.jsx
@@ -11,19 +11,11 @@ function LandingPage() {
         const handleScroll = () => {
           const sections = document.querySelectorAll('.section');
           let currentSection = 'home';
-<<<<<<< HEAD
     
           sections.forEach((section) => {
             const rect = section.getBoundingClientRect(); // Get the section's position relative to the viewport
     
             // Check if the section is in the viewport at all
-=======
-        
-          sections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-            console.log(`Section ${section.id}: `, rect); // Log the position of each section
-        
->>>>>>> 317c10131681380ed960efe6df7e9905f2e128b8
             if (rect.top < window.innerHeight && rect.bottom >= 0) {
               section.classList.add('in-view');
               currentSection = section.getAttribute('id');
@@ -31,11 +23,7 @@ function LandingPage() {
               section.classList.remove('in-view');
             }
           });
-<<<<<<< HEAD
     
-=======
-        
->>>>>>> 317c10131681380ed960efe6df7e9905f2e128b8
           setActiveSection(currentSection);
         };
     
