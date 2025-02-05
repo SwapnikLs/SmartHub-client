9e97095ea63bb10150c9315adf11dd504254d7c3
Charan Sai <csai4490@gmail.com>
2025-02-05 10:59:41 +0530

    Router is added

--- SmartHub/src/App.jsx
+++ SmartHub/src/App.jsx
@@ -1,9 +1,20 @@
 import LandingPage from './Pages/LandingPage';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Login from './Pages/Login';
 function App() {
-  
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<LandingPage/>
+    },
+    {
+      path:"Login",
+      element:<Login/>
+    }
+  ])
   return (
     <>
-      <LandingPage/>
+    <RouterProvider router={router} />
     </>
   );
 }
